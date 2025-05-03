57cff0dab831f525c5612f721a5a63109c45d430
rezasalsa28 <ezasalsabila25@gmail.com>
2025-05-03 12:07:41 +0700

    Add files via upload

---
+++ PBOreza2401091027/src/latihan10042025/StudentdRecordMain.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan10042025;
+
+/**
+ *
+ * @author ASUS
+ */
+public class StudentdRecordMain {
+   public static void main(String[] args){
+        Academic academic = new Academic();
+        System.out.println("Nama:" + academic.getName());
+        System.out.println("Course:" + academic.course);
+        System.out.println("Grade:" + academic.grade);
+    }
+  
+}
