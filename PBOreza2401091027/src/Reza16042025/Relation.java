26221691a6ccd4733ee1f02196de06ef48e7d243
rezasalsa28 <ezasalsabila25@gmail.com>
2025-05-03 11:59:32 +0700

    Add files via upload

---
+++ PBOreza2401091027/src/Reza16042025/Relation.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reza16042025;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface  Relation {
+    public boolean isGreaterWide (Object a, Object b);
+    public boolean isLessWide( Object a, Object b);
+    public boolean isEqualWide( Object a, Object b);
+    
+    public boolean isGreaterArround (Object x, Object y);
+    public boolean isLessArround( Object x, Object y);
+    public boolean isEqualArround( Object x, Object y);
+   
+
+}
