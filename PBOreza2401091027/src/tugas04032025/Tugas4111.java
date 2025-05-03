b4f31b142705b20da62322fd11439e2fe7faf5df
rezasalsa28 <ezasalsabila25@gmail.com>
2025-05-03 12:09:21 +0700

    Add files via upload

---
+++ PBOreza2401091027/src/tugas04032025/Tugas4111.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reza26022025;
+
+/**
+ *
+ * @author LAB-MM
+ */
+public class Tugas4111 {
+    public static void main( String[] args ){
+        int number= 10;
+        char letter= 'a';
+        boolean result=true;
+        String str ="hello";
+
+        System.out.println(" number = " +number);
+        System.out.println(" letter = " +letter);
+        System.out.println(" result = " +result);
+        System.out.println(" str = " +str);
+    }
+}
