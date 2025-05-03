2801a4f61471250064065e8f50cc944135040d75
rezasalsa28 <ezasalsabila25@gmail.com>
2025-05-03 11:21:27 +0700

    Add files via upload

---
+++ PBOreza2401091027/src/reza26022025/OperatorKondisi2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reza26022025;
+
+/**
+ *
+ * @author ASUS
+ */
+public class OperatorKondisi2 {
+     public static void main(String[] args){
+        int score = 0; 
+        char answer = 'a'; 
+ 
+        score = (answer == 'a') ? 10 : 0; 
+        System.out.println("Score = " + score );
+    }
+
+    
+}
